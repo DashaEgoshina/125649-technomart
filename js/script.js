ad040eb367174bb19929a1486070aebc3a177063
DashaEgoshina <89033424777@mail.ru>
2016-02-19 17:35:59 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -92,24 +92,3 @@ window.addEventListener("keydown", function(event) {
 
 
 
-var actionOpen = document.querySelector(".btn.buy");
-var actionPopup = document.querySelector(".modal-content");
-var actionClose = actionPopup.querySelector(".modal-content-close");
-
-actionOpen.addEventListener("click", function(event) {
-    event.preventDefault();
-    actionPopup.classList.add("modal-content-show");
-});
-
-actionClose.addEventListener("click", function(event) {
-    event.preventDefault();
-    actionPopup.classList.remove("modal-content-show");
-});
-
-window.addEventListener("keydown", function(event) {
-    if (event.keyCode === 27) {
-        if (actionPopup.classList.contains("modal-content-show")) {
-            actionPopup.classList.remove("modal-content-show");
-        }
-    }
-});
